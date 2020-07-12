49d850b6115d629b48ace779e1bb555b85138523
Steven West <Smwest87@gmail.com>
2020-07-12 10:16:05 -0500

    Added error handling, converted return values into pointers

--- dice.go
+++ dice.go
@@ -1,36 +1,48 @@
 package Dice
 
 import (
+	"errors"
 	"math/rand"
 	"sort"
 	"time"
 )
 
 //RollDie -- need to find a way to randomize independent of time
-func RollDie(n int) int {
-	time.Sleep(5)
+func RollDie(n int) (*int, error) {
+	if n < 1 {
+		err := errors.New("Die size cannot be 0 or negative")
+		return nil, err
+	}
+	time.Sleep(100 * time.Millisecond)
 	randomSeed := rand.NewSource(time.Now().UnixNano())
 	randomNumberGenerator := rand.New(randomSeed)
-	min := 1
-	max := n
+	max := n + 1
 	if n == 1 {
-		return n
+		return &n, nil
 	}
-	result := randomNumberGenerator.Intn(max-min) + min
-	return result
+	result := randomNumberGenerator.Intn(max)
+	if result == 0 {
+		result = 1
+		return &result, nil
+	}
+	return &result, nil
 }
 
-func RollStat() int {
-	var list [4]int
+//RollStat -- rolls the stat for players. Roll 4d6 and drop the lowest value
+func RollStat() (*int, error) {
+	var list []int
 	total := 0
-	list[0] = RollDie(6)
-	list[1] = RollDie(6)
-	list[2] = RollDie(6)
-	list[3] = RollDie(6)
 
-	sort.Ints(list[:])
+	for i := 0; i < 4; i++ {
+		roll, err := RollDie(6)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, *roll)
+	}
 
+	sort.Ints(list)
 	total = list[1] + list[2] + list[3]
 
-	return total
+	return &total, nil
 }
--- dice_test.go
+++ dice_test.go
@@ -13,14 +13,25 @@ func TestRollDie(t *testing.T) {
 		randomSeed := rand.NewSource(time.Now().UnixNano())
 		randomGenerator := rand.New(randomSeed)
 		n := randomGenerator.Intn(20)
-		result := RollDie(n)
-		assert.Greater(tt, result, 0)
-		assert.Less(tt, result, 21)
+		result, err := RollDie(n)
+		assert.Nil(tt, err)
+		assert.Greater(tt, *result, 0)
+		assert.Less(tt, *result, 21)
 
 	})
 	t.Run("n := 1 ", func(tt *testing.T) {
 		n := 1
-		result := RollDie(n)
-		assert.Equal(tt, 1, result)
+		result, err := RollDie(n)
+		assert.Nil(tt, err)
+		assert.Equal(tt, 1, *result)
+	})
+}
+
+func TestRollStat(t *testing.T) {
+	t.Run("Success", func(tt *testing.T) {
+		result, err := RollStat()
+		assert.Nil(tt, err)
+		assert.Greater(tt, *result, 2)
+		assert.Less(tt, *result, 19)
 	})
 }
