1b6ca89ea9207164118849afe36987190a7cc428
Steven West <Smwest87@gmail.com>
2020-07-13 21:43:25 -0500

    Added test that actually use the rand.Rand applied in the struct. Need to mock rand.Rand instead

--- dice_test.go
+++ dice_test.go
@@ -1,9 +1,7 @@
 package dice
 
 import (
-	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,10 +9,8 @@ import (
 func TestRollDie(t *testing.T) {
 	roller := NewRoller()
 	t.Run("Valid Integer -- Success", func(tt *testing.T) {
-		randomSeed := rand.NewSource(time.Now().UnixNano())
-		randomGenerator := rand.New(randomSeed)
-		n := randomGenerator.Intn(20)
-		result, err := roller.RollDie(n, randomGenerator)
+		n := roller.RNG.Intn(20)
+		result, err := roller.RollDie(n, roller.RNG)
 		assert.Nil(tt, err)
 		assert.Greater(tt, result, 0)
 		assert.Less(tt, result, 21)
@@ -23,9 +19,7 @@ func TestRollDie(t *testing.T) {
 
 	t.Run("n := 1 ", func(tt *testing.T) {
 		n := 1
-		randomSeed := rand.NewSource(time.Now().UnixNano())
-		randomGenerator := rand.New(randomSeed)
-		result, err := roller.RollDie(n, randomGenerator)
+		result, err := roller.RollDie(n, roller.RNG)
 		assert.Nil(tt, err)
 		assert.Equal(tt, 1, result)
 	})
@@ -33,9 +27,7 @@ func TestRollDie(t *testing.T) {
 	t.Run("Failure, n < 1", func(tt *testing.T) {
 		n := 0
 		expectedErrorMessage := "Die size cannot be 0 or negative"
-		randomSeed := rand.NewSource(time.Now().UnixNano())
-		randomGenerator := rand.New(randomSeed)
-		result, err := roller.RollDie(n, randomGenerator)
+		result, err := roller.RollDie(n, roller.RNG)
 		assert.Equal(tt, -1, result)
 		assert.Equal(tt, expectedErrorMessage, err.Error())
 	})
